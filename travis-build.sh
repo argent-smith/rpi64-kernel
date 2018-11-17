ac2cd35c1ec209586a906f16c038963af1b6ec4c
Pavel Argentov <argentoff@gmail.com>
2018-11-17 22:23:23 +0300

    Revert "Removed unnecessary (?) env var"
    This reverts commit c0c0a7aa8f9991c3e2c8de1b1f9fdbbb5aa6b2ae.

--- travis-build.sh
+++ travis-build.sh
@@ -2,8 +2,13 @@
 set -e
 
 # This script is meant to run on Travis-CI only
-if [ -z "$TRAVIS_BRANCH" ]; then
+if [ -z "$TRAVIS_BRANCH" ]; then 
   echo "ABORTING: this script runs on Travis-CI only"
+  exit 1
+fi
+# Check essential envs
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "ABORTING: env GITHUB_TOKEN is missing"
   exit 1
 fi
 
